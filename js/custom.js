682d7d6f69fbadc356871d34c2ffb8b68f4bd5d0
Matt Carrick <mattcarrick13@gmail.com>
2016-11-14 09:30:25 +0000

    moved JS around and added draggable divs

---
+++ js/custom.js
@@ -0,0 +1,68 @@
+$(document).ready(function() {
+    $('#colorpicker').farbtastic('#colorvalue');
+
+    $('.farbtastic').click(function() {
+        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec($('#colorvalue').val());
+        r = parseInt(result[1], 16);
+        g = parseInt(result[2], 16);
+        b = parseInt(result[3], 16);
+        $("#rvalue").val(r);
+        $("#gvalue").val(g);
+        $("#bvalue").val(b);
+
+        console.log([r, g, b]);
+        r /= 255, g /= 255, b /= 255;
+        var max = Math.max(r, g, b), min = Math.min(r, g, b);
+        var h, s, l = (max + min) / 2;
+
+        if(max == min){
+            h = s = 0; // achromatic
+        }else{
+            var d = max - min;
+            s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+            switch(max){
+                case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+                case g: h = (b - r) / d + 2; break;
+                case b: h = (r - g) / d + 4; break;
+            }
+            h /= 6;
+        }
+
+        $("#hvalue").val(Math.round(h * 100) / 100);
+        $("#svalue").val(Math.round(s * 100) / 100);
+        $("#lvalue").val(Math.round(l * 100) / 100);
+    });
+
+    $(".rgbvalue").change(function() {
+        if(parseInt($(this).val()) < 0 || parseInt($(this).val()) > 255) {
+            return;
+        }
+
+        r = parseInt($("#rvalue").val());
+        g = parseInt($("#gvalue").val());
+        b = parseInt($("#bvalue").val());
+
+        hexr = ("00" + r.toString(16)).substr(-2);
+        hexg = ("00" + g.toString(16)).substr(-2);
+        hexb = ("00" + b.toString(16)).substr(-2);
+
+        $.farbtastic('#colorpicker').setColor('#'+hexr+hexg+hexb);
+    });
+
+    $(".hslvalue").change(function() {
+        if(parseFloat($(this).val()) < 0 || parseFloat($(this).val()) > 1) {
+            return;
+        }
+
+        h = parseFloat($("#hvalue").val());
+        s = parseFloat($("#svalue").val());
+        l = parseFloat($("#lvalue").val());
+
+        $.farbtastic('#colorpicker').setHSL([h,s,l]);
+        console.log($.farbtastic('#colorpicker').hsl);
+    });
+});
+
+$( function() {
+    $( ".drag" ).draggable();
+} );
