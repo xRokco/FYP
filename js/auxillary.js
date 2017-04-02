df1a7399ed4f8d12b7af0c0742533f0dcd2759bf
Matt Carrick <mattcarrick13@gmail.com>
2017-04-02 16:58:32 +0100

    added startswith for IE

--- js/auxillary.js
+++ js/auxillary.js
@@ -749,4 +749,11 @@ $(document).ready(function() {
     }
 
     init();
+
+    if (!String.prototype.startsWith) {
+        String.prototype.startsWith = function(searchString, position) {
+            position = position || 0;
+            return this.indexOf(searchString, position) === position;
+        };
+    }
 });
