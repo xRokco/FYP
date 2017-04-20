7e0b1bb997a8fccba2c0fd031390cb6527014faf
Matt Carrick <mattcarrick13@gmail.com>
2017-04-20 14:26:08 +0100

    finished commenting, updated readme

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # FYP
 HTML5 image editing with the canvas element
+
+## Installation
+Put the entire project on a webserver and visit index.html in a modern web browser (Chrome works best)
+
+For testing purposes, you can use the simple Python HTTP server to serve the files -
+On Linux, cd to the project directory and run `python -m SimpleHTTPServer` and then visit `localhost:8000` in your web browser.
+
+On windows do the same but run `python -m http.server` and then go to `localhost:8000`
+
+Otherwise just put it in a web server like Apache
+
+## My code
+Everything in the project was written by me, except the following libraries
+* farbtastic.js - for the colour wheel - https://github.com/mattfarina/farbtastic
+* fabric.js - the main canvas library - http://fabricjs.com/
+* skeleton.css - the main CSS file - http://getskeleton.com/
+* normalize.css - another Skeleton CSS file
+
+## Live version
+A live version of the site can be visited at http://Stijl.cf
+The most recent code can be found at https://github.com/xRokco/Stijl.cf
--- js/modal.js
+++ js/modal.js
@@ -9,11 +9,11 @@
  * On mouseout, fade them both out
  */
 $('#keyboard-icon').hover(function(){
-    $('#keyboardModal').fadeIn(800);
-    $('#keyboardModal .modal-content').fadeIn(800);
+    $('#keyboardModal').fadeIn(800); //fade in modal background
+    $('#keyboardModal .modal-content').fadeIn(800); //fade in modal window
 },function(){
-    $('#keyboardModal').fadeOut(800);
-    $('#keyboardModal .modal-content').fadeOut(800);
+    $('#keyboardModal').fadeOut(800); //fade out modal background
+    $('#keyboardModal .modal-content').fadeOut(800); //fade out modal window
 });
 
 /*
@@ -23,6 +23,9 @@ $('#exportButton').click(function(){
     $("#exportModal").show();
 });
 
+/*
+ * On document load, check if their userAgent includes the string "Mobi" and display the mobile warning modal if so
+ */
 $(document).ready(function(){
     if (/Mobi/.test(navigator.userAgent)) {
         $("#mobileModal").show();
@@ -33,13 +36,13 @@ $(document).ready(function(){
  * On click on the export button in the file menu, display the export modal
  */
 $('#filterButton').click(function(){
-    $("#filterModal").show();
-    if(!canvas.backgroundImage) {
-        $("#filter-warning").show();
-        $(".background-filter-options input").prop('disabled', true);
-    } else {
-        $("#filter-warning").hide();
-        $(".background-filter-options input").prop('disabled', false);
+    $("#filterModal").show(); //show the background image filter modal
+    if(!canvas.backgroundImage) { //if there's no background image filter
+        $("#filter-warning").show(); //display a warning informing users there's no background image
+        $(".background-filter-options input").prop('disabled', true); //disable the checkboxes
+    } else { //if there is a backgroun image
+        $("#filter-warning").hide(); //hide the warning
+        $(".background-filter-options input").prop('disabled', false); //enable the checkboxes
     }
 });
 
@@ -49,27 +52,37 @@ $('#filterButton').click(function(){
  * open the image on the canvas in a new tab/window
  */
 $('.export').click(function(){
-    if($(this).attr("value")=="json"){
-        document.getElementById('json').innerHTML = 'Loading...';
-        $("#jsonModal").show();
-        setTimeout(function() {
+    if($(this).attr("value")=="json"){ //if JSON export is clicked
+        document.getElementById('json').innerHTML = 'Loading...'; //put a loading warning in the json field
+        $("#jsonModal").show(); //show the json modal
+        setTimeout(function() { //use an async function to display the JSON
+            //temporarily change zoom level to 1
             var zoom = canvas.getZoom();
             resetZoom();
 
+            //generate the JSON and display it
             document.getElementById('json').innerHTML = JSON.stringify(canvas.toJSON(['width', 'height', 'id', 'flipX2', 'flipY2']), null, 4);
 
+            //revert zoom to the previous value
             fixZoom(zoom);
         }, 0);
     } else {
+        //temporarily change zoom level to 1
         var zoom = canvas.getZoom();
         resetZoom();
 
+        //Open the exported image in a new tab
         window.open(canvas.toDataURL( {format: $(this).attr("value") }));
 
+        //revert zoom to the previous value
         fixZoom(zoom);
     }
 });
 
+/*
+ * When the "Select All" button is clicked on in the JSON modal
+ * Select/highlight all the JSON code in the window
+ */
 $('#select').click(function(){
     var text = document.getElementById('json');
     if (document.body.createTextRange) {
@@ -85,37 +98,46 @@ $('#select').click(function(){
     } 
 });
 
+/*
+ * When the import menu button is clicked show the import the modal
+ */
 $('#importButton').click(function(){
     $("#importModal").show();
 });
 
+/*
+ * When the import button on the import modal is clicked, try imprt the user JSON
+ */
 $('#import').click(function(){
-    var json = document.getElementById('importJSON').value;
-    try {
+    var json = document.getElementById('importJSON').value; //get the JSON from the textarea
+    try { //try to parse the json
         var object = JSON.parse(json);
-    } catch(err){
+    } catch(err){ //if json doesn't parse return an error
         alert("There seems to be a problem with your JSON. Make sure it's formatted correctly and try again. Sorry about that");
         return;
     }
-    console.log("importing")
-    try {
-        if("objects" in object && "background" in object && "width" in object && "height" in object) {
-            canvas.loadFromJSON(json, function(){
-                canvas.renderAll.bind(canvas);
-                canvas.setWidth(object.width);
+    //console.log("importing")
+    
+    try { //try import the json again
+        if("objects" in object && "background" in object && "width" in object && "height" in object) { //if the json doesn't have atleast those properties, return an error
+            canvas.loadFromJSON(json, function(){ //Load the canvas from JSON
+                canvas.renderAll.bind(canvas); //render changes
+                canvas.setWidth(object.width); //Set the canvas size
                 canvas.setHeight(object.height);
-                initWidth = canvas.getWidth();
+                initWidth = canvas.getWidth(); //change the initial size
                 initHeight = canvas.getHeight();
-                $("#canvasWrapper").width(canvas.getWidth());
-                var obj = canvas.getObjects();
-                for(i=obj.length - 1; i >= 0;i--){
-                    if(obj[i].flipX2 == true && obj[i].flipY2 == true){
+                $("#canvasWrapper").width(canvas.getWidth()); //fix the background pattern
+                var obj = canvas.getObjects(); //get all objects
+                for(i=obj.length - 1; i >= 0;i--){ //loop though all objects
+                    if(obj[i].flipX2 == true && obj[i].flipY2 == true){ //If both flip vars are true do nothing
                         //do nothing
-                    }else if(obj[i].flipY2 == true || obj[i].flipX2 == true){
-                        console.log("flipping")
+                    }else if(obj[i].flipY2 == true || obj[i].flipX2 == true){ //if only one of them are true flip the object
+                        //console.log("flipping")
                         obj[i].flipY = true;
                     }
                 }
+
+                //fix the zoom if the new canvas is bigger than the screen
                 if(canvas.getWidth() > $(window).width()) {
                     canvas.setZoom(($(window).width()-300)/canvas.getWidth());
                     canvas.setDimensions({
@@ -135,31 +157,39 @@ $('#import').click(function(){
                     document.getElementById("zoom").innerHTML = "Zoom level: " + Math.round(canvas.getZoom() * 100)/100;
                     $("#canvasWrapper").width(canvas.getWidth());
                 }
-                canvas.renderAll();
-                resetPan();
-                updateLayers();
-                $('.close').click();
+
+                canvas.renderAll(); //render changes
+                resetPan(); //reset canvas to center
+                updateLayers(); //update layer window
+                $('.close').click(); //close the JSON modal
             });
-        } else {
+        } else { //if there's an error in the json, throw error
             throw("You may have badly formatted JSON, please make sure it has objects, background, width and height properties");
         }
     } catch(err) {
-        alert(err);
+        alert(err); //display the error
     }
 })
 
-//Export Modal
+/*
+ * When the new canvas button in the menu is clicked show the new canvas modal
+ */
 $('#newCanvasButton').click(function(){
-    $('#bgcolour').val(canvas.backgroundColor);
+    $('#bgcolour').val(canvas.backgroundColor); //get the current background colour and set the text box to be this
     $("#newCanvasModal").show();
 });
 
+/*
+ * When the newCanvas button in the newcanvasmodal is clicked create a new canvas
+ */
 $('#newCanvas').click(function(){
-    newCanvas(document.getElementById('width').value,document.getElementById('height').value);
+    newCanvas(document.getElementById('width').value,document.getElementById('height').value); //create new canvas with these dimensions passed in
 });
 
+/*
+ * When the resizeButton is clicked in the menu, get the resize settings and how the modal.
+ */
 $('#resizeButton').click(function(){
-    console.log("here");
     if(canvas.backgroundColor){
         document.getElementById('resizeBgcolour').value = canvas.backgroundColor;
         document.getElementById('resizeTransparent').checked = false;  
@@ -174,10 +204,16 @@ $('#resizeButton').click(function(){
     $("#resizeModal").show();
 });
 
+/*
+ * When the resize canvas function in the resize modal is clicked, resize the canvas.
+ */
 $('#resizeCanvas').click(function(){
-    resizeCanvas(document.getElementById('resizeWidth').value,document.getElementById('resizeHeight').value);
+    resizeCanvas(document.getElementById('resizeWidth').value,document.getElementById('resizeHeight').value); //resize the canvas with these new dimensions
 });
 
+/*
+ * Whenever the width is changed in the resize modal, if the maintain aspect ratio is checked, change the height by the same ratio
+ */
 $('#resizeWidth').on('change keydown paste input', function() {
     if(document.getElementById('resizeLocked').checked == true){
         if(document.getElementById('resizeType').value == 'px'){
@@ -188,6 +224,9 @@ $('#resizeWidth').on('change keydown paste input', function() {
     }
 });
 
+/*
+ * Whenever the height is changed in the resize modal, if the maintain aspect ratio is checked, change the width by the same ratio
+ */
 $('#resizeHeight').on('change keydown paste input', function() {
     if(document.getElementById('resizeLocked').checked == true){
         if(document.getElementById('resizeType').value == 'px'){
@@ -198,7 +237,9 @@ $('#resizeHeight').on('change keydown paste input', function() {
     }
 });
 
-// When the user clicks on <span> (x), close the modal
+/*
+ * When the X icon is clicked, close the modal
+ */
 $('.close').click(function() {
     $("#exportModal").hide();
     $("#newCanvasModal").hide();
@@ -208,11 +249,16 @@ $('.close').click(function() {
     $("#filterModal").hide();
 });
 
+/*
+ * When the X icon is clicked on the json modal, close it
+ */
 $('.closeJSON').click(function() {
     $("#jsonModal").hide();
 });
 
-// When the user clicks anywhere outside of the modal, close it
+/*
+ * When the user clicks anywhere outside of the modal, close it
+ */
 $(window).click(function(event) {
     if (event.target == document.getElementById('exportModal')) {
         $("#exportModal").hide();
@@ -237,6 +283,9 @@ $(window).click(function(event) {
     }
 });
 
+/*
+ * Disable the form submissions
+ */
 $('.form').submit(function(e) {
     e.preventDefault();
 });
